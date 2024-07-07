1ef73b8020f8dd9523e4682d6e50bbf7c56b3d87
Mohamed Farhun <removed@example.com>
2024-07-07 11:16:53 -0300

    Added Chat API integration, updated packages, tested local setup, and added server.js to root directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/api/:path*", // Proxy to Backend
+      },
+    ];
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['konsta'] = path.resolve(__dirname, 'node_modules/konsta');
+    return config;
+  },
 };
 
 export default nextConfig;
